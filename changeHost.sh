7fe760eb9b6d4c3a371b578c23f8a925e1397eab
SureshUppelli <45890987+SureshUppelli@users.noreply.github.com>
2021-07-22 11:48:43 +0530

    Update changeHost.sh

--- changeHost.sh
+++ changeHost.sh
@@ -2,6 +2,6 @@
 
 sed -i "s/host/$1/g" inventory
 
-sed -i "s/host-user/$2/g" inventory
+sed -i "s/hostuser/$2/g" inventory
 
-sed -i "s/host-password/$3/g" inventory
+sed -i "s/hostpassword/$3/g" inventory
